5c071f2fd999144c748a35aaa3ea9d064d9e15c3
Alexander Hurd <hurdad@gmail.com>
2023-12-19 23:52:47 -0500

    ddl update

--- clickhouse/a2s.sql
+++ clickhouse/a2s.sql
@@ -1,32 +1,32 @@
 CREATE DATABASE IF NOT EXISTS a2s;
 
 CREATE TABLE IF NOT EXISTS a2s.info (
-timestamp default now(),
-address LowCardinality(String),
-server_name LowCardinality(String),
-map_name LowCardinality(String),
-game LowCardinality(String),
-player_count UInt32,
-max_players UInt32,
-bot_count UInt32,
-server_type LowCardinality(String),
-platform LowCardinality(String),
-password_protected UInt8,
-vac_enabled UInt8,
-version LowCardinality(String),
-ping Float32
+    timestamp DateTime DEFAULT now(),
+    address LowCardinality(String),
+    server_name LowCardinality(String),
+    map_name LowCardinality(String),
+    game LowCardinality(String),
+    player_count UInt8,
+    max_players UInt8,
+    bot_count UInt8,
+    server_type LowCardinality(String),
+    platform LowCardinality(String),
+    password_protected UInt8,
+    vac_enabled UInt8,
+    version LowCardinality(String),
+    ping Float32
 )
 ENGINE = MergeTree
-ORDER BY (address, timestamp)
+ORDER BY (timestamp, address)
 TTL timestamp + toIntervalDay(7);
 
 CREATE TABLE IF NOT EXISTS  a2s.players (
-timestamp DateTime default now(),
-address LowCardinality(String),
-name String,
- score UInt32,
- duration Float32
+    timestamp DateTime DEFAULT now(),
+    address LowCardinality(String),
+    name String,
+    score UInt32,
+    duration Float32
 )
 ENGINE = MergeTree
-ORDER BY (address, timestamp)
+ORDER BY (timestamp, address)
 TTL timestamp + toIntervalDay(7);
